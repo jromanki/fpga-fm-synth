765c42a9023bb2b19af3c629afcd9d480fc9309b
jromanki <01178427@pw.edu.pl>
2026-04-16 00:30:31 +0200

    wotking test

--- src/mixer.v
+++ src/mixer.v
@@ -8,17 +8,17 @@ module mixer (
 
     // We need 48 bits to hold 32-bit * 16-bit
     reg signed [48:0] full_product;
-
-    wire signed [32:0] sum;
-    assign sum = {a0[31], a0} + {a1[31], a1};
+    reg signed [32:0] sum;
+    
 
     always @(posedge clk) begin
+        sum <= (a0 >>> 1) + (a1 >>> 1);
 
         // Use $signed(b) to ensure the multiplier treats the whole operation 
         // as a signed math problem.
         full_product <= sum * $signed({1'b0, b}); 
         
         // Scale back down (divide by 65536)
-        y <= full_product[48:16];
+        y <= full_product[47:16];
     end
 endmodule
